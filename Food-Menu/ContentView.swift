c1fdc77d867cfb3368653d05748b2ca8450edfb7
Bandit Silachai <mbandit001@yahoo.co.th>
2023-12-14 13:29:30 +0700

    Initial Commit

---
+++ Food-Menu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food-Menu
+//
+//  Created by Bandit Silachai on 14/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Food-Menu/Food_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_MenuApp.swift
+//  Food-Menu
+//
+//  Created by Bandit Silachai on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Food_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
